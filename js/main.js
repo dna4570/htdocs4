c4e629025d2e0c4cbcf4b535775004ecbcb24071
dna4570 <dna4570@gmail.com>
2020-04-21 19:19:17 +0900

    seven commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $('.slider').slick(
     dots: true;
 });
 
+$('.single-item').slick(
+    dots: true;
+);
+
 $(document).ready(function () {
     $('.your-class').slick({
         setting- name: setting - value
@@ -14,19 +18,51 @@ $(function () {
             $header = $(this),
         headerOffsetTop = $header.offset().top;
 
-    $window.on('scroll', function () {
-        if ($window.scrollTop() > headerOffsetTop)
-            $header.addClass('sticky');
-        } else {
-        $header.removeClass('stickey');
-        }
-        });
 
-        $window.trigger('scroll');
 
-    });
-});
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-$('.single-item').slick(
-    dots: true;
-);
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    } else {
+    navbar.classList.remove("sticky");
+    }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
