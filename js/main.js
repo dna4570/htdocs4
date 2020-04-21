c2e0f6bdd3b30e22f18049fa31b08587ee0190f7
dna4570 <dna4570@gmail.com>
2020-04-21 15:30:58 +0900

    jquery commits

--- js/main.js
+++ js/main.js
@@ -1,38 +1,28 @@
+$('.slider').slick(
+    dots: true;
+});
+
+$(document).ready(function () {
+    $('.your-class').slick({
+        setting- name: setting - value
+    });
+});
+
 $(function () {
+    $('.page-header').each(function () {
+        var $window =$(window),
+            $header = $(this),
+        headerOffsetTop = $header.offset().top;
 
-    /*
-     * Slideshow
-     */
-    // slideshow クラスを持った要素ごとに処理を実行
-    $('.slideshow').each(function () {
-
-        var $slides = $(this).find('img'), // すべてのスライド
-            slideCount = $slides.length,   // スライドの点数
-            currentIndex = 0;              // 現在のスライドを示すインデックス
-
-        // 1 番目のスライドをフェードインで表示
-        $slides.eq(currentIndex).fadeIn();
-
-        // 7500 ミリ秒ごとに showNextSlide 関数を実行
-        setInterval(showNextSlide, 7500);
-
-        // 次のスライドを表示する関数
-        function showNextSlide () {
-
-            // 次に表示するスライドのインデックス
-            // (もし最後のスライドなら最初に戻る)
-            var nextIndex = (currentIndex + 1) % slideCount;
-
-            // 現在のスライドをフェードアウト
-            $slides.eq(currentIndex).fadeOut();
-
-            // 次のスライドをフェードイン
-            $slides.eq(nextIndex).fadeIn();
-
-            // 現在のスライドのインデックスを更新
-            currentIndex = nextIndex;
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > headerOffsetTop)
+            $header.addClass('sticky');
+        } else {
+        $header.removeClass('stickey');
         }
+        });
+
+        $window.trigger('scroll');
 
     });
-
 });
